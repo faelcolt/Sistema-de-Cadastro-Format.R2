ddb7e1da11d5ef9035cbdacf575d8679011f281d
faelcolt <fael_colt@hotmail.com>
2022-10-01 17:59:36 -0300

    att

--- telainicial.py
+++ telainicial.py
@@ -6,6 +6,7 @@ class Application():
     def __init__(self):
         self.root = root
         self.tela()
+        self.frames_da_tela()
         root.mainloop()
     def tela(self):
         self.root.title('CADASTRO DE CLIENTES')
@@ -15,7 +16,7 @@ class Application():
         self.root.maxsize(width=988, height=788)
         self.root.minsize(width=400, height=300)
     def frames_da_tela(self):
-        self.frame_1 = Frame(self.root)
-        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.09, relheight=0.5)
+        self.frame_1 = Frame(self.root, bd=4, bg='#dfe3ee', highlightbackground='#759fe6')
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
 
 Application()
