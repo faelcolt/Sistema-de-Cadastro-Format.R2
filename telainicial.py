1a2fc4b1790ba12c571e4ebb839be85a3e8665b9
faelcolt <fael_colt@hotmail.com>
2022-10-01 19:18:55 -0300

    dados de entrada

--- telainicial.py
+++ telainicial.py
@@ -20,8 +20,8 @@ class Aplicacao():
         self.janela.configure(background= '#708090')
         self.janela.geometry("850x550")
         self.janela.resizable(True, True)
-        self.janela.maxsize(width= 880, height= 680)
-        self.janela.minsize(width=550, height=380)
+        self.janela.maxsize(width= 900, height= 700)
+        self.janela.minsize(width=500, height=400)
         
     def frames(self):
         self.frame1 = Frame(self.janela, bd = 4, bg = '#BEBEBE', highlightbackground= 'black', highlightthickness=3 )
@@ -52,9 +52,31 @@ class Aplicacao():
         self.bt_limpar = Button(self.frame1, text='APAGAR')
         self.bt_limpar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
         
+        #################################################################################
+        
         # Entrada de Dados:
         self.lb_codigo = Label(self.frame1, text='CÓDIGO')   
         self.lb_codigo.place(relx=0.05, rely=0.05)
+        self.codigo_entry = Entry(self.frame1)
+        self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
+        
+        # Entrada de Nome:
+        self.lb_nome = Label(self.frame1, text='NOME')   
+        self.lb_nome.place(relx=0.05, rely=0.35)
+        self.nome_entry = Entry(self.frame1)
+        self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.80)
+        
+        # Entrada de Contato:
+        self.lb_nome = Label(self.frame1, text='TELEFONE')   
+        self.lb_nome.place(relx=0.05, rely=0.6)
+        self.nome_entry = Entry(self.frame1)
+        self.nome_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
+        
+        # Entrada de Endereço:
+        self.lb_nome = Label(self.frame1, text='ENDEREÇO')   
+        self.lb_nome.place(relx=0.05, rely=0.6)
+        self.nome_entry = Entry(self.frame1)
+        self.nome_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
         
 
 # Chamando a Função:        
