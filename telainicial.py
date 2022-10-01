418da14dbcc1b102e1894591300b256bf9716b93
faelcolt <fael_colt@hotmail.com>
2022-10-01 17:50:07 -0300

    att

--- telainicial.py
+++ telainicial.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 
-class aplication():
+class Application():
     def __init__(self):
         self.root = root
         self.tela()
@@ -13,6 +13,9 @@ class aplication():
         self.root.geometry('788x588')
         self.root.resizable(True, True)
         self.root.maxsize(width=988, height=788)
-        self.root.minsize(width=550, height=550)
+        self.root.minsize(width=400, height=300)
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root)
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.09, relheight=0.5)
 
-aplication()
+Application()
