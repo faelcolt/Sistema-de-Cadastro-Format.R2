5e9b8e665039b55348bc9c92c1e52104e8e5c173
faelcolt <fael_colt@hotmail.com>
2022-10-01 22:50:56 -0300

    Att da Identação

--- SysCad-FormatR2.py
+++ SysCad-FormatR2.py
@@ -1,10 +1,21 @@
 from tkinter import*
 from tkinter import ttk
+from tkinter import font
 
 
 root=Tk()
 
-class Application():
+class Funcs():
+    def limpa_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.email_entry.delete(0, END)
+        self.tel_entry.delete(0, END)
+        self.cep_entry.delete(0, END)
+        self.cnpj_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
+
+class Application(Funcs):
     def __init__(self):
         
         self.root=root
@@ -28,7 +39,7 @@ class Application():
     def frames_da_tela(self):
         
         self.frame_1=Frame(self.root, bd=4, bg="lightcyan", highlightbackground="gray", highlightthickness=4)
-        self.frame_1.place(relx=0.02,rely=0.02,relwidth=0.96,relheight=0.46)
+        self.frame_1.place(relx=0.02,rely=0.02, relwidth=0.96, relheight=0.46)
 
 
         self.frame_2=Frame(self.root, bd=4, bg="lightcyan", highlightbackground="gray", highlightthickness=4)
@@ -36,27 +47,27 @@ class Application():
 
 
     def widgets_frame1(self):
-        self.bt_limpar=Button(self.frame_1,text="Limpar",bd=3,bg="#107db2",fg="white", font=("verdana",8,"bold"))
+        self.bt_limpar=Button(self.frame_1, text="Limpar",bd=3, bg="white", fg="black", font=("verdana",8,"bold"), command=self.limpa_tela)
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
 
-        self.bt_buscar=Button(self.frame_1,text="Buscar",bd=3,bg="#107db2",fg="white", font=("verdana",8,"bold"))
+        self.bt_buscar=Button(self.frame_1, text="Buscar", bd=3,bg="#107db2", fg="white", font=("verdana",8,"bold"))
         self.bt_buscar.place(relx=0.31, rely=0.1, relwidth=0.1, relheight=0.15)
 
 
-        self.bt_buscar=Button(self.frame_1,text="Novo",bd=3,bg="cadetblue3",fg="white", font=("verdana",8,"bold"))
+        self.bt_buscar=Button(self.frame_1, text="Novo", bd=3, bg="cadetblue3", fg="white", font=("verdana",8,"bold"))
         self.bt_buscar.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
 
 
-        self.bt_buscar=Button(self.frame_1,text="Alterar",bd=3,bg="#107db2",fg="white", font=("verdana",8,"bold"))
+        self.bt_buscar=Button(self.frame_1, text="Alterar",bd=3, bg="#107db2", fg="white", font=("verdana",8,"bold"))
         self.bt_buscar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
 
 
-        self.bt_buscar=Button(self.frame_1,text="Apagar",bd=3,bg="tomato",fg="white", font=("verdana",8,"bold"))
+        self.bt_buscar=Button(self.frame_1, text="Apagar", bd=3, bg="tomato", fg="white", font=("verdana",8,"bold"))
         self.bt_buscar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 
         # criaçao da label e entrada do codigo
 
-        self.lb_codigo=Label(self.frame_1, text="::: Código :::",bg="lightcyan",fg="#107db2")
+        self.lb_codigo=Label(self.frame_1, text="::: Código :::", bg="lightcyan", fg="black")
         self.lb_codigo.place(relx=0.06, rely= 0.07)
 
         self.codigo_entry=Entry(self.frame_1,bg="ivory2")
@@ -64,7 +75,7 @@ class Application():
 
         # criacao da label e entrada do nome
 
-        self.lb_nome=Label(self.frame_1, text="::: Nome :::",bg="lightcyan",fg="#107db2")
+        self.lb_nome=Label(self.frame_1, text="::: Nome :::", bg="lightcyan", fg="black")
         self.lb_nome.place(relx=0.07, rely= 0.35)
 
         self.nome_entry=Entry(self.frame_1,bg="ivory2")
@@ -73,7 +84,7 @@ class Application():
 
         # criacao da label e entrada do E-mail
 
-        self.lb_email=Label(self.frame_1, text="::: E-mail :::",bg="lightcyan",fg="#107db2")
+        self.lb_email=Label(self.frame_1, text="::: E-mail :::", bg="lightcyan", fg="black")
         self.lb_email.place(relx=0.5, rely= 0.35)
 
         self.email_entry=Entry(self.frame_1,bg="ivory2")
@@ -82,7 +93,7 @@ class Application():
 
         #criacao da label e entrada do telefone
 
-        self.lb_tel=Label(self.frame_1, text="::: Telefone :::",bg="lightcyan",fg="#107db2")
+        self.lb_tel=Label(self.frame_1, text="::: Telefone :::", bg="lightcyan", fg="black")
         self.lb_tel.place(relx=0.07, rely= 0.6)
 
         self.tel_entry=Entry(self.frame_1,bg="ivory2")
@@ -90,7 +101,7 @@ class Application():
 
         #criacao da label e entrada do cep
 
-        self.lb_cep=Label(self.frame_1, text="::: CEP :::",bg="lightcyan",fg="#107db2")
+        self.lb_cep=Label(self.frame_1, text="::: CEP :::", bg="lightcyan", fg="black")
         self.lb_cep.place(relx=0.3, rely= 0.6)
 
         self.cep_entry=Entry(self.frame_1,bg="ivory2")
@@ -98,7 +109,7 @@ class Application():
 
         #criacao da label e entrada do CNPJ
 
-        self.lb_cnpj=Label(self.frame_1, text="::: CNPJ :::",bg="lightcyan",fg="#107db2")
+        self.lb_cnpj=Label(self.frame_1, text="::: CNPJ :::", bg="lightcyan", fg="black")
         self.lb_cnpj.place(relx=0.56, rely= 0.6)
 
         self.cnpj_entry=Entry(self.frame_1,bg="ivory2")
@@ -107,7 +118,7 @@ class Application():
 
         #criacao da label e entrada da cidade
 
-        self.lb_cidade=Label(self.frame_1, text="::: Cidade :::",bg="lightcyan",fg="#107db2")
+        self.lb_cidade=Label(self.frame_1, text="::: Cidade :::", bg="lightcyan", fg="black")
         self.lb_cidade.place(relx=0.8, rely= 0.6)
 
         self.cidade_entry=Entry(self.frame_1,bg="ivory2")
